6531d11507927f9e0c677690efbe70024e1ab680
M.M <mmendes090@live.com>
2022-11-08 16:20:22 -0300

    first commit

---
+++ my-log-app-test/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using my_log_app_test.Models;
+
+namespace my_log_app_test.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        _logger.LogWarning("Teste Log Warning - Index - Home Controller ");
+        _logger.LogError("Teste Log Error - Index - Home Controller ");
+
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        _logger.LogWarning("Teste Log Warning - Privacy - Home Controller ");
+        _logger.LogError("Teste Log Error - Privacy - Home Controller ");
+
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
---
+++ my-log-app-test/Models/Log.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace my_log_app_test.Models
+{
+    public class Log
+    {
+        public string Message { get; set; }
+
+        public Log(string message)
+        {
+            this.Message = message;
+        }
+    }
+}
+
